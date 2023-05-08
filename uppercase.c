97d65143bcc541750c548d4247fe4c3b26c69635
TomasNagy <tomi.nagy@outlook.com>
2023-05-08 11:13:45 +0200

    Implement lowercase to uppercase algorithm

--- uppercase.c
+++ uppercase.c
@@ -4,8 +4,24 @@
 int main(void)
 {
 	//Get string from user
+	string name = get_string("What's your name? ");
 
 	//Change lowercase letters to uppercase
+	//Iterate through the string character by character until NUL
+	int n = 0;
+	
+	while (name[n] != '\0')
+	{
+		//Characters between 'a' and 'z' are lowercase
+		if (name[n] >= 'a' && name[n] <= 'z')
+		{
+			//Add 32 to the lowercase character to convert it to uppercase
+			name[n] -= 32;
+		}
+
+		n++;
+	}
 
 	//Print out the result
+	printf("%s\n", name);
 }
