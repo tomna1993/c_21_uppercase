267abec780f6ff6afb8c2e7e0b6375907779aed0
TomasNagy <tomi.nagy@outlook.com>
2023-05-08 11:19:13 +0200

    Abstract lowercase to uppercase algorithm to a separate function

--- uppercase.c
+++ uppercase.c
@@ -1,27 +1,37 @@
 #include <cs50.h>
 #include <stdio.h>
 
+string lowerToUpperCase(string text);
+
 int main(void)
 {
 	//Get string from user
 	string name = get_string("What's your name? ");
 
 	//Change lowercase letters to uppercase
-	//Iterate through the string character by character until NUL
+	name = lowerToUpperCase(name);
+
+	//Print out the result
+	printf("%s\n", name);
+}
+
+//Function to change lowercase letters to uppercase in a given text
+string lowerToUpperCase(string text)
+{
 	int n = 0;
-	
-	while (name[n] != '\0')
+
+	//Iterate through the string character by character until NUL
+	while (text[n] != '\0')
 	{
 		//Characters between 'a' and 'z' are lowercase
-		if (name[n] >= 'a' && name[n] <= 'z')
+		if (text[n] >= 'a' && text[n] <= 'z')
 		{
 			//Add 32 to the lowercase character to convert it to uppercase
-			name[n] -= 32;
+			text[n] -= 32;
 		}
 
 		n++;
 	}
 
-	//Print out the result
-	printf("%s\n", name);
+	return text;
 }
