ccb177a299b2c45c1d6abcc5e6dc353ba3a38820
TomasNagy <tomi.nagy@outlook.com>
2023-05-08 11:38:40 +0200

    Implement lowercase to uppercase algorithm

--- uppercase.c
+++ uppercase.c
@@ -1,11 +1,26 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
 	//Get string from user
+	string s = get_string("Before: ");
+
+	printf("After:  ");
 
 	//Change lowercase letters to uppercase
+	for (int i = 0; i < strlen(s); i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			printf("%c", s[i] - 32);
+		}
+		else
+		{
+			printf("%c", s[i]);
+		}
+	}
 
-	//Print out the result
+	printf("\n");
 }
