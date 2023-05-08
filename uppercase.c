5f22006a341cde9e7cbb8a01d12378a6650a030c
TomasNagy <tomi.nagy@outlook.com>
2023-05-08 11:29:43 +0200

    Optimize lowerToUpperCase function with using strlen from string.h library

--- uppercase.c
+++ uppercase.c
@@ -1,34 +1,32 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 string lowerToUpperCase(string text);
 
 int main(void)
 {
 	//Get string from user
-	string name = get_string("What's your name? ");
+	string s = get_string("Before: ");
 
 	//Change lowercase letters to uppercase
 	//Print out the result
-	printf("%s\n", lowerToUpperCase(name));
+	printf("After:  ");
+	printf("%s\n", lowerToUpperCase(s));
 }
 
 //Function to change lowercase letters to uppercase in a given text
 string lowerToUpperCase(string text)
 {
-	int n = 0;
-
 	//Iterate through the string character by character until NUL
-	while (text[n] != '\0')
+	for (int i = 0; i < strlen(text); i++)
 	{
 		//Characters between 'a' and 'z' are lowercase
-		if (text[n] >= 'a' && text[n] <= 'z')
+		if (text[i] >= 'a' && text[i] <= 'z')
 		{
 			//Add 32 to the lowercase character to convert it to uppercase
-			text[n] -= 32;
+			text[i] -= 32;
 		}
-
-		n++;
 	}
 
 	return text;
